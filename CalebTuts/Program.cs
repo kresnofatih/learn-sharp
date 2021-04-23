7f90fe4f36a72e86b6c5b354104f8445b60bd744
Fatih <kresnofatihimani.private@gmail.com>
2021-04-23 16:21:21 +0700

    created class, instantiated an object, and field

--- CalebTuts/Program.cs
+++ CalebTuts/Program.cs
@@ -6,52 +6,20 @@ namespace CalebTuts // a namespace - used for organization
     class Program // a class - has members (such as a method)
     {
         static void Main(string[] args)
-            // a method - contains a block of code, Main is the starting point
-            // this is creating the method, you will need to invoke a method later.
-            // For Main, it will not require us to invoke
+        // a method - contains a block of code, Main is the starting point
+        // this is creating the method, you will need to invoke a method later.
+        // For Main, it will not require us to invoke
         {
-            // lists
-            List<int> grades = new List<int>() { 5, 19 };
-            List<List<int>> nestedLists = new List<List<int>>()
-            {
-                new List<int>{5, 6, 7},
-                new List<int>{34, 20, 39, 28}
-            };
-            grades.Add(20);
-            grades.Insert(2, 15);
+            Program myProgram = new Program(); //instantiate
+            myProgram.printName();
+        }
 
-            Console.WriteLine(grades[0]); //5
-            Console.WriteLine(grades[1]);//19
-            Console.WriteLine(grades[2]);//15
-            Console.WriteLine(grades[3]);//20
+        public void printName()
+        {
+            User me = new User();
+            me.Name = "Kresno Fatih";
 
-            //grades.Clear();
-
-            // for loop for lists
-            for(int i = 0; i<grades.Count; i++)
-            {
-                Console.WriteLine(grades[i]);
-            }
-
-            // for each for lists
-            foreach (int grade in grades)
-            {
-                Console.WriteLine(grade);
-            }
-
-            foreach(List<int> nestedList in nestedLists)
-            {
-                foreach(int num in nestedList)
-                {
-                    Console.Write(num+" ");
-                }
-                Console.WriteLine();
-            }
-
-            int[] myArr = grades.ToArray();// convert to array
-
-            //sort
-            grades.Reverse(); // 20 15 19 5
+            Console.WriteLine(me.Name);
         }
     }
 }
---
+++ CalebTuts/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalebTuts
+{
+    public class User
+    {
+        // fields: defined at the class level (not the recommended way to store information)
+        public string Name;
+
+    }
+}
