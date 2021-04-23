4b9b5c6b42433f048a9b2b8b1bdfb56f175087b8
Fatih <kresnofatihimani.private@gmail.com>
2021-04-23 10:01:13 +0700

    classes and methods

--- CalebTuts/Program.cs
+++ CalebTuts/Program.cs
@@ -9,18 +9,10 @@ namespace CalebTuts // a namespace - used for organization
             // this is creating the method, you will need to invoke a method later.
             // For Main, it will not require us to invoke
         {
-            Int32 x = 5; // .Net framework variables
-            int y = (int) 3.99;// rounds down to 3
-            Console.WriteLine(x);//5
-            Console.WriteLine(y);//3
-
-            float z = 5.5f; // float literals is different
-            double p = 5.5; // bigger information
-            decimal q = 39.4M; // double literals
-            double r = double.NaN;
-
-            Console.WriteLine(double.IsNaN(p));
-            Console.WriteLine(double.IsNaN(r));
+            Person person = new Person();
+            person.FirstName = "Kresno";
+            person.LastName = "Fatih";
+            Console.WriteLine(person.GetFullName()); // Kresno Fatih
         }
     }
 }
