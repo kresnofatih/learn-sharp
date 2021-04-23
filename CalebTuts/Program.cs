ac18f01c5dd751ff0582a45b591106ab0dec3195
Fatih <kresnofatihimani.private@gmail.com>
2021-04-23 13:52:49 +0700

    list loops

--- CalebTuts/Program.cs
+++ CalebTuts/Program.cs
@@ -20,7 +20,19 @@ namespace CalebTuts // a namespace - used for organization
             Console.WriteLine(grades[2]);//15
             Console.WriteLine(grades[3]);//20
 
-            grades.Clear();
+            //grades.Clear();
+
+            // for loop for lists
+            for(int i = 0; i<grades.Count; i++)
+            {
+                Console.WriteLine(grades[i]);
+            }
+
+            // for each for lists
+            foreach (int grade in grades)
+            {
+                Console.WriteLine(grade);
+            }
         }
     }
 }
