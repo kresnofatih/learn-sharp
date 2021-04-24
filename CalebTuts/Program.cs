928e1ac74dd359f2c01a6c7fdfe760e6a6bc262b
Fatih <kresnofatihimani.private@gmail.com>
2021-04-24 14:42:49 +0700

    inheritance

--- CalebTuts/Program.cs
+++ CalebTuts/Program.cs
@@ -10,57 +10,17 @@ namespace CalebTuts // a namespace - used for organization
         // this is creating the method, you will need to invoke a method later.
         // For Main, it will not require us to invoke
         {
-            Program myProgram = new Program(); //instantiate
-            myProgram.PrintName();
+            Program myProgram = new Program();
+            myProgram.DoSomething();
         }
 
-        public void PrintName()
+        public void DoSomething()
         {
-            User you = new User();
-            you.FirstName = "Kresno";// will override the firstname default value to Irham
-            you.LastName = "Fatih";
-            Console.WriteLine(you.FullName); // kresno fatih
-
-            User me = new User();
-            me.FullName = "Kresno Fatih";
-            Console.WriteLine(you.FullName); // kresno fatih
-
-            User him = new User();
-            him.FullName = "Fatih";
-            Console.WriteLine(him.FullName); // fatih fatih
-
-            Console.WriteLine("");
-
-            List<User> users = new List<User>();
-            users.Add(me);
-            users.Add(you);
-            users.Add(him);
-
-            foreach(User user in users)
-            {
-                Console.WriteLine(user.FullName);
-                //kresno fatih
-                //kresno fatih
-                //fatih fatih
-            }
-
-            User.PrintListOfUserFullName(users);
-
-            Console.WriteLine(User.FindUser(users, "kresno fatih"));//true
-
-            TakeUser(me);
-            Console.WriteLine(me.FullName);//jamesfatih
-
-            User.PrintUserFullName(me);// for static methods are implemented on the class directly
-
-            Console.WriteLine("");
-            Console.WriteLine(me.IntroduceYourself()); // default value takes place
-            Console.WriteLine(me.IntroduceYourself("Kyoto, Osaka")); // default value overridden
-        }
-
-        public void TakeUser(User user)
-        {
-            user.FirstName = "James";
+            Student me = new Student();
+            me.FirstName = "Kresno";
+            me.LastName = "Fatih";
+            me.Verified = true;
+            Console.WriteLine(me.FullName);
         }
     }
 }
--- CalebTuts/User.cs
+++ CalebTuts/User.cs
@@ -8,81 +8,16 @@ namespace CalebTuts
 {
     public class User
     {
-        // fields: defined at the class level (not the recommended way to store information)
-        string _firstName = "FirstName";
-        string _lastName = "LastName";
-
-        // properties: gateway to our fields
-        public string FirstName
-        {
-            get
-            {
-                return _firstName;
-            }
-            set
-            {
-                _firstName = value.ToLower();
-            }
-        } // if get & set is not a set of code, autoamtically creates a field, if set, then it connects to a field
-        public string LastName
-        {
-            get
-            {
-                return _lastName;
-            }
-            set
-            {
-                _lastName = value.ToLower();
-            }
-        }
         public string FullName
         {
             get
             {
                 return FirstName + " " + LastName;
             }
-            set
-            {
-                if(value.Split(" ").Length > 1)
-                {
-                    _firstName = value.Split(" ")[0].ToLower();
-                    _lastName = value.Split(" ")[1].ToLower();
-                } else
-                {
-                    _firstName = value.ToLower();
-                    _lastName = value.ToLower();
-                }
-            }
         }
 
-        public static void PrintUserFullName(User user)
-        {
-            Console.WriteLine(user.FullName);
-        }
-
-        public static void PrintListOfUserFullName(List<User> users)
-        {
-            foreach(User user in users)
-            {
-                Console.WriteLine(user.FullName);
-            }
-        }
-
-        public string IntroduceYourself(string homeTown="Tokyo, Japan")// instead of overloading, use default values
-        {
-            return "My name is " + FullName+". I am from "+homeTown;
-        }
-
-        public static bool FindUser(List<User> users, string fullName)
-        {
-            foreach(User user in users)
-            {
-                if(user.FullName == fullName)
-                {
-                    return true;
-                }
-            }
-            return false;
-        }
+        public string FirstName { get; set; } = "FirstName";
+        public string LastName { get; set; } = "LastName";
+        public bool Verified { get; set; } = false; // by default the property value is false
     }
 }
